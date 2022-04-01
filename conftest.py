d6083683e2ced39b35b6538994fd6b5d65f8a902
Nathan Collins <colli372@msu.edu>
2022-04-01 14:28:36 -0400

    Move module into src/

--- conftest.py
+++ conftest.py
@@ -3,6 +3,9 @@ Config file executed before pytest runs tests
 """
 import os
 import sys
-cur_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, cur_path)
+src_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "src/"
+)
+sys.path.insert(0, src_path)
 
--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setuptools.setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
     ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.9",
     install_requires=[
         "Cerberus >= 1.3",
